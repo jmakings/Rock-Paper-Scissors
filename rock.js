6a0454cdfca93776efbe5c50f9f4c7c633fb38a3
Jeff Makings <jeffmakings@gmail.com>
2023-01-18 19:53:44 -0800

    Finish game function

--- rock.js
+++ rock.js
@@ -1,6 +1,7 @@
-
+// Generates random number from 1 to 3 to generate 
+// the computer's selection
 function getComputerChoice() { 
-    let value = Math.floor(Math.random() *3 ) + 1
+    let value = Math.floor(Math.random() * 3) + 1
     let ans = ''
     if (value == 1)
         ans = 'ROCK'
@@ -23,16 +24,54 @@ function oneRound(playerSelection, computerSelection) {
     if ((player == 'ROCK' && cmptr == 'SCISSORS') ||
      (player == 'PAPER' && cmptr == 'ROCK') || 
      (player == 'SCISSORS' && cmptr == 'PAPER')) {
-        console.log('You Win! ' + player + ' beats ' + cmptr)
+        console.log('You win this round! ' + player + ' beats ' + cmptr)
         ans = 1
     }
     else if (player == cmptr) {
-        console.log('Draw! Both you and the computer selected ' + player)
+        console.log('This round is a draw! Both you and the computer selected ' + player)
         ans = 3
     }
     else {
-        console.log('You lose! ' + cmptr + ' beats ' + player)
+        console.log('You lose this round! ' + cmptr + ' beats ' + player)
         ans = 2
     }
     return ans
 }   
+
+// Simulates a game of RPS with a set number of rounds
+function game(rounds) {
+
+    // To count wins, losses, and draws for the player
+    let playW = 0
+    let compW = 0
+    let draw = 0
+
+    for (let i = 0; i < rounds; i++) {
+        let compSelect = getComputerChoice()
+        let playSelect = prompt("Input Rock, Paper, or Scissors", "Rock")
+
+        let ans = oneRound(playSelect, compSelect)
+
+        if (ans == 1) {
+            playW = playW + 1
+        }
+        else if (ans == 2) {
+            compW = compW + 1
+        }
+        else {
+            draw = draw + 1
+        }
+    }
+    if (playW > compW) {
+        console.log('You WIN the game!! The final tally was ' + playW + ' wins, ' + 
+        compW + ' losses, and ' + draw + ' draws.')
+    }
+    else if (playW < compW) {
+        console.log('You LOSE the game!! The final tally was ' + playW + ' wins, ' + 
+        compW + ' losses, and ' + draw + ' draws.')
+    }
+    else {
+        console.log('The game is a DRAW!! The final tally was ' + playW + ' wins, ' + 
+        compW + ' losses, and ' + draw + ' draws.')
+    }
+}
