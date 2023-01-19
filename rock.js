f183c083fc3bcd3310b172ea1074d8e98cb5bf17
Jeff Makings <jeffmakings@gmail.com>
2023-01-18 18:59:03 -0800

    add GetComputerChoice() function

--- rock.js
+++ rock.js
@@ -1,4 +1,14 @@
 
-function getComputerChoice() {
-    return Math.floor(Math.random() *3 ) + 1
+function getComputerChoice() { 
+    let value = Math.floor(Math.random() *3 ) + 1
+    let ans = ''
+    if (value == 1)
+        ans = 'Rock'
+    else if (value == 2)
+        ans = 'Paper'
+    else 
+        ans = 'Scissors'
+    
+    return ans
 }
+
